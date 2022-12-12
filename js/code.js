e628a22a16fa1fcff46fba8f851434ebc332e1e1
timvma <timawbi@gmail.com>
2022-12-12 11:23:09 +0900

    update

--- js/code.js
+++ js/code.js
@@ -4,38 +4,35 @@ const templates = document.querySelectorAll('template')
 
 
 function createCode(template) {
-    let parent = template.parentElement
+  
     let preELement = document.createElement('pre')
     let codeElement = document.createElement('code')
-    
+    let copyButtonElement = document.createElement('button')
+    copyButtonElement.setAttribute('class','copy-code')
+    copyButtonElement.textContent = 'Copy'
+
+    copyButtonElement.addEventListener('click', ()=> {
+        navigator.clipboard.writeText(codeElement.textContent);
+        copyButtonElement.textContent ='Copied'
+    })
+
+    preELement.addEventListener('mouseleave', ()=> {
+        copyButtonElement.textContent ='Copy'
+    })
+
+    preELement.append(copyButtonElement)
+
     codeElement.textContent = template.innerHTML
     
-
-
     preELement.append(codeElement)
-    parent.append(preELement)
+    
+    return preELement
 }
-
 
 if(templates)
 templates.forEach(template => {
-    //template.nextElementSibling.textContent = template.innerHTML 
-    createCode(template)
-});
-
-
-const copyCode = document.querySelectorAll('.copy-code')
-
-if(copyCode)
-copyCode.forEach(btn => {
-    btn.addEventListener('click', ()=> {
-        navigator.clipboard.writeText(btn.previousElementSibling.textContent);
-        btn.textContent ='Copied'
-    })
-
-    btn.parentElement.addEventListener('mouseleave', ()=> {
-        btn.textContent ='Copy'
-    })
+    let code = createCode(template)
+    template.parentElement.append(code)
 });
 
 
